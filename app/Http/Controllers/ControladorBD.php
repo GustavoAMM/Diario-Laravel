854c1fd1bf28a7fcf64a5ae842676ae293f73137
Gustavo Montoya <99113181+GustavoAMM@users.noreply.github.com>
2022-11-28 22:24:34 -0600

    add validations

--- app/Http/Controllers/ControladorBD.php
+++ app/Http/Controllers/ControladorBD.php
@@ -81,7 +81,7 @@ class ControladorBD extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(validadorDiario $request, $id)
     {
         DB::table('tb_recuerdos')->where('idRecuerdo',$id)->update([
             "titulo"=>$request->input('txtTitulo'),
